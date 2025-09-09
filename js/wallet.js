718669dd11f487801ee900c148069997f7aede3f
OfficialMCL <officialmcleague@gmail.com>
2025-09-09 14:29:04 +0200

    Update wallet.js

--- js/wallet.js
+++ js/wallet.js
@@ -7,9 +7,16 @@ const connectWallet = async () => {
       const resp = await window.solana.connect();
       console.log('Wallet connected:', resp.publicKey.toString());
       alert('Wallet connected: ' + resp.publicKey.toString());
-      // You can now use resp.publicKey for further operations
+      // You can use resp.publicKey for further operations
     } catch (err) {
-      console.error('Connection rejected or failed', err);
+      if (err.code === 4001) {
+        // User closed or rejected the connection request
+        console.log('Wallet connection cancelled by the user.');
+        alert('Wallet connection cancelled. Please try again by clicking the button.');
+      } else {
+        console.error('Error during connection:', err);
+        alert('An error occurred while connecting to the wallet.');
+      }
     }
   } else {
     alert('Phantom Wallet is not installed. Please install it from https://phantom.app/');
