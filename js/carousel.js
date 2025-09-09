2f297ccdb1f8b216db270705e38dd506e8e60f64
OfficialMCL <officialmcleague@gmail.com>
2025-09-09 14:46:53 +0200

    Create carousel.js

---
+++ js/carousel.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  autoplay: { delay: 7000, disableOnInteraction: false },
+  navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
+  pagination: { el: '.swiper-pagination', clickable: true },
+});
+
+const pauseBtn = document.getElementById('pauseBtn');
+const playBtn = document.getElementById('playBtn');
+
+pauseBtn.addEventListener('click', () => {
+  swiper.autoplay.stop();
+  pauseBtn.style.display = 'none';
+  playBtn.style.display = 'inline-block';
+});
+
+playBtn.addEventListener('click', () => {
+  swiper.autoplay.start();
+  playBtn.style.display = 'none';
+  pauseBtn.style.display = 'inline-block';
+});
