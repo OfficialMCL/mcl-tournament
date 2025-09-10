f354e6251a8ee67bd884a2038508056a288ba317
OfficialMCL <officialmcleague@gmail.com>
2025-09-10 02:10:21 +0200

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -1,18 +1,16 @@
 // carousel.js - Carousel functionality for Swiper
 const swiper = new Swiper('.swiper', {
   loop: true,
-  autoplay: { 
-    delay: 8000, 
-    disableOnInteraction: false 
+  // NON includere autoplay
+  // autoplay: { delay: 8000, disableOnInteraction: false },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
-  navigation: { 
-    nextEl: '.swiper-button-next', 
-    prevEl: '.swiper-button-prev' 
-  },
-  pagination: { 
-    el: '.swiper-pagination', 
-    clickable: true 
-  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  }
 });
 
 // Circular Play/Pause button control
